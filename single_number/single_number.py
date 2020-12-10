d49dd3f5fdb144c1aa1bd8472ccd97114d04ab3e
Fatima Rizvi <fatimarzv23@gmail.com>
2020-12-10 10:49:46 -0600

    Completed single number program and moving zeroes. There are multiple solutions in the moving zeroes file

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,35 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
+# Requirement: O(1) space complexity
 
-    pass
+def single_number(arr):
+    # Create a counter variable dictionary
+    # Count frequency of numbers 
+    # Search through the counter to find the frequency of 1
+    # Return number that has a frequency of one
+    frequency = {}
+    for num in arr:
+        # If num is not in the counter dictionary yet, then make it's value 1
+        if num not in frequency:
+            frequency[num] = 1
+        # If num is already in counter, increment count by one
+        else:
+            frequency[num] += 1
+    
+    for num, count in frequency.items():
+        if count == 1:
+            return num
+
+    #Alt
+    # Sort Array in place
+    # Look check if first 2 match, then check the next two until they don't match
+    # If they don't match, return the first number
+    # If it's the last item, the loop will complete before it covers the last item, so we return the last item
+
+    #Note: .count() seems to be O(n) for space/time complexity
+
+    
 
 
 if __name__ == '__main__':
